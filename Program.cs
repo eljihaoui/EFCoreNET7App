e2662417c2f9d4e8f61f153bd3a101bab674cf7d
MOHAMED EL JIHAOUI <eljihaoui@gmail.com>
2023-09-03 12:40:59 +0100

    OrderBy

--- Program.cs
+++ Program.cs
@@ -2,8 +2,4 @@
 using static System.Console;
 // OrderBy
 using var _context = new BookDbContext();
-var books = _context.Book.OrderByDescending(b=>b.PublishedOn).ThenByDescending(b=>b.Price);
-foreach (var book in books)
-{
-    WriteLine($"- Year : {book.PublishedOn}  Price : {book.Price} Title : {book.Title} ");
-}
+
